b2e04eda85669573677df93ac60099f2378cf972
JohnB4000 <115543417+JohnB4000@users.noreply.github.com>
2025-12-03 21:27:00 +0000

    Complete day 3

--- src/days/day03.rs
+++ src/days/day03.rs
@@ -1,26 +1,77 @@
-use std::{fs::File, io::BufReader};
-
 use super::Day;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 pub struct Day03;
 
 impl Day for Day03 {
-    type ParsedData = (Vec<i32>, Vec<i32>);
+    type ParsedData = Vec<String>;
 
-    type Part1Result = i32;
+    type Part1Result = u64;
 
-    type Part2Result = i32;
+    type Part2Result = u64;
 
     fn parse(input_directory: String) -> Result<Self::ParsedData, String> {
-        let reader = BufReader::new(File::open(input_directory + "day01.txt").expect("File Error"));
-        todo!()
+        let reader = BufReader::new(File::open(input_directory + "day03.txt").expect("File Error"));
+
+        Ok(reader.lines().map(|l| l.unwrap()).collect())
     }
 
     fn part_1(input: &Self::ParsedData) -> Self::Part1Result {
-        0
+        input.iter().fold(0, |a, s| {
+            let chars = s.chars().collect::<Vec<char>>();
+            let mut highest = 0;
+            for i in 0..chars.len() {
+                for j in (i + 1)..chars.len() {
+                    let num = [chars[i], chars[j]]
+                        .iter()
+                        .copied()
+                        .collect::<String>()
+                        .parse::<u64>()
+                        .unwrap();
+                    if num > highest {
+                        highest = num;
+                    }
+                }
+            }
+            a + highest
+        })
     }
 
     fn part_2(input: &Self::ParsedData) -> Self::Part2Result {
-        0
+        input.iter().fold(0, |a, s| {
+            let chars = s.chars().collect::<Vec<char>>();
+
+            let mut number: Vec<char> = Vec::new();
+
+            let mut start = 0;
+            let mut end = chars.len() - 12 + 1;
+
+            while number.len() < 12 {
+                let mut num_max = 0;
+                let mut num_pos = 0;
+                for (i, c) in chars[start..end].iter().enumerate() {
+                    let d = c.to_digit(10).unwrap();
+                    if d > num_max {
+                        num_max = d;
+                        num_pos = i;
+                    }
+                }
+                number.push(chars[start + num_pos]);
+                start += 1 + num_pos;
+                end += 1;
+
+                if end > chars.len() {
+                    while number.len() < 12 {
+                        number.push(chars[start]);
+                        start += 1;
+                    }
+                    break;
+                }
+            }
+            a + number.iter().collect::<String>().parse::<u64>().unwrap()
+        })
     }
 }
